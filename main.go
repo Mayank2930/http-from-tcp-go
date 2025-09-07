b23ee50807e70074ac7b8661d1386eee2602e9f4
root <root@Mayank.localdomain>
2025-09-07 22:00:33 +0530

    reading from a file

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"fmt"
+	"log"
+)
 
 func  main()  {
-	fmt.Println("I hope I got the job!")
+	f, err := os.Open("message.txt")
+	if err != nil {
+		log.Fatal("Error", err)
+	}
+
+	for {
+		data := make([]byte, 8)
+		n, err := f.Read(data)
+		if err != nil {
+			break
+		}
+
+		fmt.Printf("read: %s\n", string(data[:n]))
+	}
 }
