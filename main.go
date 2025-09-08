8590a3b48ed8f4c51e6aaa6375e744b3ed62471f
Mayank2930 <bhardwajmayank1916@gmail.com>
2025-09-08 23:11:38 +0530

    refactored to use a function that reads lines

--- main.go
+++ main.go
@@ -5,7 +5,41 @@ import (
 	"fmt"
 	"log"
 	"bytes"
+	"io"
 )
+
+func getLinesChannel(f io.ReadCloser) <- chan string {
+	out := make(chan string, 1)
+
+	go func(){
+		defer f.Close()
+		defer close(out)
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i + 1:]
+				out <- str
+				str = ""
+			}
+
+			str += string(data)
+		}
+
+		if len(str) != 0 {
+			out <- str
+		}
+	}()
+
+	return out
+}
 
 func  main()  {
 	f, err := os.Open("message.txt")
@@ -14,26 +48,9 @@ func  main()  {
 	}
 	defer f.Close()
 
-	str := ""
-	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
-		if err != nil {
-			break
-		}
+	lines := getLinesChannel(f)
 
-		data = data[:n]
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			str += string(data[:i])
-			data = data[i + 1:]
-			fmt.Printf("read: %s\n", str)
-			str = ""
-		}
-
-		str += string(data)
-	}
-
-	if len(str) != 0 {
-		fmt.Printf("read: %s\n", str)
+	for line := range lines{
+		fmt.Printf("read: %s\n", line)
 	}
 }
